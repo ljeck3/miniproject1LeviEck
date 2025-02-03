cd7482603e4671f425131408432292f7db6ca026
levieck <ljeck3@mail.fhsu.edu>
2025-02-03 15:01:42 -0600

    Created the requirements.txt and started our initial Python file called main.py.

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 ### INF601 - Advanced Programming in Python
 ### Levi Eck
 ### Mini Project 1
+import pprint
+import yfinance as yf
+
+msft = yf.Ticker("MSFT")
+
+# get all stock info
+#pprint.pprint(msft.info)
+
+#get historical market data
+hist = msft.history(period='1mo')
+
+pprint.pprint(hist)

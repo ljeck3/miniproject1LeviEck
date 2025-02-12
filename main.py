03fdfb0849233057ae5956b3d0b413fc49af4c55
levieck <ljeck3@mail.fhsu.edu>
2025-02-12 13:41:05 -0600

    Final edits and cleanup

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 ### INF601 - Advanced Programming in Python
 ### Levi Eck
 ### Mini Project 1
+
+#Package Imports
 import yfinance as yf
 from datetime import datetime, timedelta
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+
 os.makedirs("charts", exist_ok=True)
 
 today = datetime.now()
 ten_days_ago = today - timedelta(days=15)
 
-
-myTickers = ["MSFT", "NTDOY", "NVDA", "GME", "AMC"]
+#Collect closing price of 5 stock tickers for the last 10 trading days
+#Store in a list that coverts to an array in NumPy
+myTickers = ["MSFT", "NTDOY", "NVDA", "TXT", "F"]
 
 myTickers.sort()
 for ticker in myTickers:
@@ -23,14 +27,14 @@ for ticker in myTickers:
     for date in hist['Close'][:11]:
         last10days.append(date)
 
-
         myarray = np.array(last10days)
         max_price = myarray.max() + (myarray.max()*.05)
         min_price = myarray.min() - (myarray.max()*.05)
+        #Plot the graphs
         plt.plot(myarray)
         plt.xlabel('Days Ago')
         plt.ylabel('Closing Price')
         plt.axis((9, 0, min_price, max_price))
         plt.title(f"{ticker} Last 10 Closing Prices")
+        #Save in a folder called charts as PNG files
         plt.savefig(f"charts/{ticker}.png")
-
